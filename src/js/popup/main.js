2ad45104bb5491aac117033a08c98f4ba142dc57
Derek Hopper <hopper.derek@gmail.com>
2012-09-10 19:00:13 -0400

    CSS animations for voting.

--- src/js/popup/main.js
+++ src/js/popup/main.js
@@ -1,7 +1,20 @@
+var img_html = "<img class=\"img-voted\"/>";
+
 $(document).ready(function() {
   
   $('.voting a').hover(function() {
     $(this).find('i').toggleClass('icon-white');
   });
+  
+  $('.voting > li a').click(function() {
+    var vote = $(this).data('vote');
+    
+    $('.voting > li a .img-voted').remove();
+    $(this).addClass('voted');
+    
+    var $img = $(img_html);
+    $img.attr('src', 'img/icons/'+vote+'.png');
+    $(this).append($img);
+  })
   
 });
